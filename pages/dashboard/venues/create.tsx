cf55eca698c59e758b816f6e66ca0ae5386d35b6
Eric <32720134+ECarlsson@users.noreply.github.com>
2022-03-12 11:43:15 +0100

    Redirect after POST

--- pages/dashboard/venues/create.tsx
+++ pages/dashboard/venues/create.tsx
@@ -1,8 +1,10 @@
 import { Field, Form, Formik } from 'formik';
 import type { InferGetServerSidePropsType, NextPage } from 'next';
-import type { Venue } from '@models/VenueModel';
+import type { Venue, VenueDocument } from '@models/VenueModel';
 import UserModel from '@models/UserModel';
 import DashboardLayout from '@components/DashboardLayout';
+import { useRouter } from 'next/router';
+import type { LeanDocument } from 'mongoose';
 
 export const getServerSideProps = async () => {
   const managers = await UserModel.find()
@@ -27,67 +29,72 @@ const initialValues: Venue = {
 
 const CreateVenue: NextPage<
 InferGetServerSidePropsType<typeof getServerSideProps>
-> = ({ managers }) => (
-  <DashboardLayout>
-    <Formik
-      initialValues={initialValues}
-      onSubmit={async (values, { setStatus }) => {
-        try {
-          const res = await fetch('/api/venues/create', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(values),
-          });
-          if (!res.ok) {
-            throw new Error(`Network error: ${res.status} ${res.statusText}`);
+> = ({ managers }) => {
+  const router = useRouter();
+  return (
+    <DashboardLayout>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={async (values, { setStatus }) => {
+          try {
+            const res = await fetch('/api/venues', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(values),
+            });
+            if (!res.ok) {
+              throw new Error(`Network error: ${res.status} ${res.statusText}`);
+            }
+            const venue: LeanDocument<VenueDocument> = await res.json();
+            router.push(`/dashboard/venues/${venue._id}`);
+          } catch (error) {
+            setStatus({ error });
           }
-        } catch (error) {
-          setStatus({ error });
-        }
-      }}
-    >
-      {({ isSubmitting, status }) => (
-        <Form className="d-flex flex-column">
-          <label htmlFor="name" className="form-label">
-            Name*
-          </label>
-          <Field type="text" id="name" name="name" required />
-          <label htmlFor="description" className="form-label mt-3">
-            Description
-          </label>
-          <Field type="text" id="description" name="description" />
-          <label htmlFor="managers" className="form-label mt-3">
-            Managers
-          </label>
-          <Field as="select" multiple id="managers" name="managers">
-            {managers.map((manager) => (
-              <option key={manager.id} value={manager.id}>
-                {manager.email}
-              </option>
-            ))}
-          </Field>
-          <div className="form-check form-switch mt-3">
-            <Field
-              type="checkbox"
-              id="enabled"
-              name="enabled"
-              className="form-check-input"
-            />
-            <label htmlFor="enabled" className="form-check-label">
-              Enabled
+        }}
+      >
+        {({ isSubmitting, status }) => (
+          <Form className="d-flex flex-column">
+            <label htmlFor="name" className="form-label">
+              Name*
             </label>
-          </div>
-          <span className="text-muted mt-3">*Mandatory fields</span>
-          <button type="submit" className="btn btn-primary mt-3" disabled={isSubmitting}>
-            Create
-          </button>
-          { status?.error && <div className="alert alert-danger mt-3">{status.error.message}</div> }
-        </Form>
-      )}
-    </Formik>
-  </DashboardLayout>
-);
+            <Field type="text" id="name" name="name" required />
+            <label htmlFor="description" className="form-label mt-3">
+              Description
+            </label>
+            <Field type="text" id="description" name="description" />
+            <label htmlFor="managers" className="form-label mt-3">
+              Managers
+            </label>
+            <Field as="select" multiple id="managers" name="managers">
+              {managers.map((manager) => (
+                <option key={manager.id} value={manager.id}>
+                  {manager.email}
+                </option>
+              ))}
+            </Field>
+            <div className="form-check form-switch mt-3">
+              <Field
+                type="checkbox"
+                id="enabled"
+                name="enabled"
+                className="form-check-input"
+              />
+              <label htmlFor="enabled" className="form-check-label">
+                Enabled
+              </label>
+            </div>
+            <span className="text-muted mt-3">*Mandatory fields</span>
+            <button type="submit" className="btn btn-primary mt-3" disabled={isSubmitting}>
+              Create
+            </button>
+            { status?.error && <div className="alert alert-danger mt-3">{status.error.message}</div> }
+          </Form>
+        )}
+      </Formik>
+    </DashboardLayout>
+  );
+};
 
 export default CreateVenue;
